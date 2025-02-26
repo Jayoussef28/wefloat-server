c547643e5b00f709e677861217344876b4203582
Jordan Youssef <jordanyoussef28@gmail.com>
2025-02-25 20:07:41 -0600

    rating model complete

---
+++ wefloatapi/models/rating.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Rating(models.Model):
+  
+    RATING_VALUES = [
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+    ]
+  
+    value = models.CharField(max_length=10, choice=RATING_VALUES, default='1')
